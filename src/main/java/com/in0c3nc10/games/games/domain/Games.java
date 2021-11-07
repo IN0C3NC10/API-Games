b6c63f46c20818895aa15cc1c7a8b7feb4249439
IN0C3NC10 <bruno.inocencio@fatec.sp.gov.br>
2021-11-07 04:11:01 -0300

    Starting APIGames and domain folder already created

---
+++ src/main/java/com/in0c3nc10/games/games/domain/Games.java
@@ -0,0 +1,89 @@
+package com.in0c3nc10.games.games.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+// ..com esta notação é criado uma tabela no database
+@Entity
+public class Games implements Serializable{
+
+    // ..é usado como boa prática o Serializable
+    private static final long serialVersionUID=1L;
+
+    // ..informa ao JPA que o "id" é uma pk, e abaixo é o método de gerar o valor
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    // ..define que ambos devem ser preenchidos, caso contrário retorna uma mensagem
+    @NotEmpty(message = "Campo NOME é obrigatório!")
+    @Length(min = 4, max = 150, message = "O campo NOME deve conter entre 3 e 150 caracteres!")
+    private String name;
+    @NotEmpty(message = "Campo CATEGORIA é obrigatório!")
+    @Length(min = 2, max = 70, message = "O campo CATEGORIA deve conter entre 2 e 70 caracteres!")
+    private String category;
+    
+    // ..construtores
+    public Games() {
+    }
+
+    public Games(Integer id, String name, String category) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+
+    // ..gets and sets
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    // ..hashcode do Id
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Games other = (Games) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
---
+++ src/test/java/com/in0c3nc10/games/games/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in0c3nc10.games.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
