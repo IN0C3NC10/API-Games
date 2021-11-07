4c644b9ce12f52a09f25de40452c6885b9ee341e
IN0C3NC10 <bruno.inocencio@fatec.sp.gov.br>
2021-11-07 17:07:34 -0300

    Implemented update method

--- src/main/java/com/in0c3nc10/games/games/controller/GamesController.java
+++ src/main/java/com/in0c3nc10/games/games/controller/GamesController.java
@@ -7,10 +7,7 @@ import com.in0c3nc10.games.games.services.GamesServices;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/games")
@@ -19,18 +16,27 @@ public class GamesController {
     @Autowired
     private GamesServices service;
 
+    // ..PESQUISAR PELO ID
     @GetMapping(value = "/{id}")
     public ResponseEntity<Games> findById(@PathVariable Integer id){
         Games obj = this.service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    // ..PESQUISAR TODOS OS DADOS
     @GetMapping
     public ResponseEntity<List<Games>> findAll(){
         // ..acessa o método da service
         List<Games> list = service.findAll();
         // retorna o ok com a lista de dados
         return ResponseEntity.ok().body(list);
+    }
+
+    // ..ALTERAR OS DADOS
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Games> update(@PathVariable Integer id, @RequestBody Games obj){
+        Games newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
     }
 
 }
--- src/main/java/com/in0c3nc10/games/games/services/GamesServices.java
+++ src/main/java/com/in0c3nc10/games/games/services/GamesServices.java
@@ -16,13 +16,26 @@ public class GamesServices {
     @Autowired
     private GamesRepository repository;
 
+    // ..PESQUISAR PELO ID
     public Games findById(Integer id){
         Optional<Games> obj = repository.findById(id);
         // ..caso encontrar ele retorna o game, se não ele retorna a throw ObjectNotFound
         return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto "+ id +" não encontrado!"));
     }
 
+    // ..PESQUISAR TODOS OS DADOS
     public List<Games> findAll() {
         return repository.findAll();
+    }
+
+    // ..ALTERAR OS DADOS
+    public Games update(Integer id, Games obj) {
+        // ..verifica se o id está presente via findById
+        Games newObj = findById(id);
+        // ..insere os dados na instância newObj
+        newObj.setName(obj.getName());
+        newObj.setCategory(obj.getCategory());
+        // ..de fato salva os dados
+        return repository.save(newObj);
     }
 }
